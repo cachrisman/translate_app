70a34334d236f0d572670e1333580c1cb15c0d52
Charlie Chrisman <charlie.chrisman@gmail.com>
2020-10-28 23:01:35 +0100

    added autoresizer support for field

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, HelpText } from '@contentful/forma-36-react-components';
+import { Button } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
 import { SingleLineEditor } from '@contentful/field-editor-single-line';
 const translate = require("deepl");
@@ -12,10 +12,6 @@ interface AppInstallationParameters {
   deeplApiKey: string;
 }
 
-interface AppInstanceParameters {
-  helpText: string;
-}
-
 interface DeepLResponse {
   data: {
     translations: {
@@ -25,43 +21,52 @@ interface DeepLResponse {
   }
 }
 
-const Field = (props: FieldProps) => {
-  const current_locale = props.sdk.field.locale
-  let default_locale = props.sdk.locales.default
+// const Field = (props: FieldProps) => {
+export default class Field extends React.Component<FieldProps> {
+  constructor(props: FieldProps) {
+    super(props);
+    this.state = {};
+  }
 
-  const translateField = () => {
-    const {deeplApiKey} = (props.sdk.parameters.installation as AppInstallationParameters)
-    const source_locale = default_locale
-    const destination_locales = props.sdk.locales.available.filter(locale => {return locale !== source_locale})
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+  }
+
+  translateField() {
+    const {deeplApiKey} = (this.props.sdk.parameters.installation as AppInstallationParameters)
+    const source_locale = this.props.sdk.locales.default
+    const destination_locales = this.props.sdk.locales.available.filter(locale => {return locale !== source_locale})
     destination_locales.forEach(destination_locale => {
       translate({
-        text: props.sdk.field.getValue(),
+        text: this.props.sdk.field.getValue(),
         source_lang: source_locale.slice(0,2).toUpperCase(),
         target_lang: destination_locale.slice(0,2).toUpperCase(),
         auth_key: deeplApiKey,
       }).then((result: DeepLResponse)  => {
         let translated_text = result.data.translations[0].text
-        let field_id = props.sdk.field.id
-        props.sdk.entry.fields[field_id].setValue(translated_text, destination_locale)
+        let field_id = this.props.sdk.field.id
+        this.props.sdk.entry.fields[field_id].setValue(translated_text, destination_locale)
         console.log(translated_text);
       }).catch((error:Error) => console.error);
     })
   }
 
-  let button = null
-  if (current_locale === default_locale) button = <Button buttonType="muted" onClick={translateField} >Translate</Button>
+  render() {
+    const current_locale = this.props.sdk.field.locale
+    const default_locale = this.props.sdk.locales.default
+    let button = null
+    if (current_locale === default_locale) button = <Button 
+      icon="Language" 
+      buttonType="muted" 
+      onClick={this.translateField} >
+        Translate
+    </Button>
 
-  const {helpText} = (props.sdk.parameters.instance as AppInstanceParameters)
-  let helptext_component = null
-  if (helpText) helptext_component = <HelpText>{ helpText }</HelpText>
-
-  return (
-    <div>
-      <SingleLineEditor field={props.sdk.field} locales={props.sdk.locales} />
-      { helptext_component }
-      { button }
-    </div>
-  )
+    return (
+      <div>
+        <SingleLineEditor field={this.props.sdk.field} locales={this.props.sdk.locales} />
+        { button }
+      </div>
+    )
+  }
 };
-
-export default Field;
