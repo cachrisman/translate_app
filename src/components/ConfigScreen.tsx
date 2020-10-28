8a15c59c9981bacf99ec9076efa75ea92d013cc0
Charlie Chrisman <charlie.chrisman@gmail.com>
2020-10-28 21:56:59 +0100

    added config/installation screen

--- src/components/ConfigScreen.tsx
+++ src/components/ConfigScreen.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { AppExtensionSDK } from 'contentful-ui-extensions-sdk';
-import { Heading, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
+import { Heading, Form, Workbench, Paragraph, TextField } from '@contentful/forma-36-react-components';
 import { css } from 'emotion';
 
-export interface AppInstallationParameters {}
+export interface AppInstallationParameters {
+  deeplApiKey: string;
+}
 
 interface ConfigProps {
   sdk: AppExtensionSDK;
@@ -16,7 +18,11 @@ interface ConfigState {
 export default class Config extends Component<ConfigProps, ConfigState> {
   constructor(props: ConfigProps) {
     super(props);
-    this.state = { parameters: {} };
+    this.state = {
+      parameters: {
+        deeplApiKey: ''
+      }
+    };
 
     // `onConfigure` allows to configure a callback to be
     // invoked when a user attempts to install the app or update
@@ -53,13 +59,26 @@ export default class Config extends Component<ConfigProps, ConfigState> {
       targetState: currentState
     };
   };
+  
+  updateApiKey = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("updateApiKey -> value", event?.target?.value)
+    this.setState({ parameters: { deeplApiKey: event.target.value }});
+  };
 
   render() {
     return (
       <Workbench className={css({ margin: '80px' })}>
         <Form>
           <Heading>App Config</Heading>
-          <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
+          <Paragraph>Welcome to the DeepL Translation App. In order to use this app, you need to have a DeepL Pro API account. <a href="https://www.deepl.com/pro#developer" target="_blank" rel="noreferrer">Click here to sign up for a new account</a> or <a href="https://www.deepl.com/pro-account.html" target="_blank" rel="noreferrer" >click here to visit your DeepL account summary</a> and copy your Authentication key. Then paste your DeepL API key below and click the Install or Save button in the upper right.</Paragraph>
+          <TextField
+            required
+            name="deeplApiKey"
+            id="deeplApiKey"
+            labelText="DeepL API Key"
+            value={this.state.parameters.deeplApiKey}
+            onChange={this.updateApiKey}
+          />
         </Form>
       </Workbench>
     );
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -2,56 +2,18 @@ import React from 'react';
 import { Button, HelpText } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
 import { SingleLineEditor } from '@contentful/field-editor-single-line';
- const translate = require("deepl");
-
-// -----------------------------------
-// POST /v2/translate?auth_key=[yourAuthKey]> HTTP/1.0
-// Host: api.deepl.com
-// User-Agent: YourApp
-// Accept: */*
-// Content-Length: 54
-// Content-Type: application/x-www-form-urlencoded
-
-// auth_key=[yourAuthKey]&text=Hello, world&target_lang=DE
-// EXAMPLE RESPONSE
-// {
-// 	"translations": [{
-// 		"detected_source_language":"EN",
-// 		"text":"Hallo, Welt!"
-// 	}]
-// }
-// -----------------------------------
-
-// const translate = require("deepl");
-//
-// translate({
-//     text: 'I am a text',
-//     target_lang: 'FR',
-//     auth_key: 'authkey',
-//     // All optional parameters available in the official documentation can be defined here as well.
-//   })
-//   .then(result => {
-//       console.log(result.data);
-//   })
-//   .catch(error => {
-//       console.error(error)
-//   });
-// RESPONSE
-// {
-//     "translations": [
-//         {
-//             "detected_source_language": "EN",
-//             "text": "Je suis un texte"
-//         }
-//     ]
-// }
+const translate = require("deepl");
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
-interface AppInstanceParameters {
+interface AppInstallationParameters {
   deeplApiKey: string;
+}
+
+interface AppInstanceParameters {
+  helpText: string;
 }
 
 interface DeepLResponse {
@@ -64,46 +26,42 @@ interface DeepLResponse {
 }
 
 const Field = (props: FieldProps) => {
+  const current_locale = props.sdk.field.locale
+  let default_locale = props.sdk.locales.default
 
   const translateField = () => {
-    const {deeplApiKey} = (props.sdk.parameters.instance as AppInstanceParameters)
-    const source_locale = props.sdk.locales.default
+    const {deeplApiKey} = (props.sdk.parameters.installation as AppInstallationParameters)
+    const source_locale = default_locale
     const destination_locales = props.sdk.locales.available.filter(locale => {return locale !== source_locale})
-    destination_locales.map(destination_locale => {
+    destination_locales.forEach(destination_locale => {
       translate({
         text: props.sdk.field.getValue(),
         source_lang: source_locale.slice(0,2).toUpperCase(),
         target_lang: destination_locale.slice(0,2).toUpperCase(),
         auth_key: deeplApiKey,
-      })
-      .then((result: DeepLResponse)  => {
-          let translated_text = result.data.translations[0].text
-          let field_id = props.sdk.field.id
-          console.log(translated_text, field_id);
-          props.sdk.entry.fields[field_id].setValue(translated_text, destination_locale)
-      })
-      .catch((error:Error) => {
-          console.error(error)
-      });
+      }).then((result: DeepLResponse)  => {
+        let translated_text = result.data.translations[0].text
+        let field_id = props.sdk.field.id
+        props.sdk.entry.fields[field_id].setValue(translated_text, destination_locale)
+        console.log(translated_text);
+      }).catch((error:Error) => console.error);
     })
-    console.log('you clicked a button')
-    
   }
-
-  console.log(props.sdk)
-  
 
   let button = null
-  if (props.sdk.field.locale === props.sdk.locales.default) {
-    button = <Button buttonType="muted" onClick={translateField} >Translate</Button>
-  }
+  if (current_locale === default_locale) button = <Button buttonType="muted" onClick={translateField} >Translate</Button>
 
-  return <div>
-    <SingleLineEditor field={props.sdk.field} locales={props.sdk.locales} />
-    <HelpText>Help Text 123</HelpText>
-    { button }
+  const {helpText} = (props.sdk.parameters.instance as AppInstanceParameters)
+  let helptext_component = null
+  if (helpText) helptext_component = <HelpText>{ helpText }</HelpText>
 
-  </div>
+  return (
+    <div>
+      <SingleLineEditor field={props.sdk.field} locales={props.sdk.locales} />
+      { helptext_component }
+      { button }
+    </div>
+  )
 };
 
 export default Field;
